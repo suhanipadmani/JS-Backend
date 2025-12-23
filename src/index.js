51d8723d85d5695539f652a8595d4c784a432886
suhanipadmani <suhanipadmani0903@gmail.com>
2025-12-23 22:01:25 +0530

    commit changes

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./.env"
+});
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`App is running on port ${process.env.PORT}`);
+    }); 
+}).catch((error) => {
+    console.error("Database connection failed:", error);
+}); 
+
+
+
+
+
+/*
+import express from "express";
+const app = express();
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        app.on("error", (error) => {
+            console.error("Error", error);
+            throw error;
+        })
+        app.listen(process.env.PORT, () => {
+            console.log(`App is running on port ${process.env.PORT}`);
+        });
+
+    } catch (error) {
+        console.error("Error:", error);
+        throw error;
+    }
+})()
+*/
